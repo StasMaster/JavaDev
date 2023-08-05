53f5bbd08e093477e6aaf135be1ab6dbf763d41d
StasMaster <56350576+StasMaster@users.noreply.github.com>
2023-08-05 02:39:57 +0200

    Delete hw2 directory

--- hw2/MyCalcProject/src/main/java/goit/Main.java
+++
@@ -1,9 +0,0 @@
-package goit;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("This is my calc!");
-        System.out.println("sum is: " + new Calculator().add(50,70));
-        System.out.println("sum is: " + new Calculator().div(50,0));
-    }
-}
