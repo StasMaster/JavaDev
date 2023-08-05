1f8edaf6e4c7bee9d981a5628ea449dab0f91c29
StasMaster <56350576+StasMaster@users.noreply.github.com>
2023-08-05 02:23:30 +0200

    Add files via upload

---
+++ hw2/MyCalcProject/src/main/java/goit/Calculator.java
@@ -0,0 +1,21 @@
+package goit;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    public int mul(int a, int b){
+        return a * b;
+    }
+
+    public int div(int a, int b){
+        if(b == 0)
+            throw new ArithmeticException("Division by zero isn't allowed");
+        return a / b;
+    }
+}
